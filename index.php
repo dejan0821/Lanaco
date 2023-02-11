cac1d92c4b15c170d964d6e5bca8723c1be02aa0
dejan0821 <banjalukahiking@gmail.com>
2023-02-11 14:10:25 +0100

    Komit1

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prodavnica</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+<div class="header-img">
+        <img src="slike/logo1.png" alt="Header Image">
+    </div>
+
+<div class="indeks-btns">
+        <form class="form-index" action="home.php">
+            <input type="submit" value="Home" />
+        </form>   
+        <form class="form-index" action="login.php">
+            <input type="submit" value="Login" />
+        </form>
+        <form class="form-index" action="register.php">
+            <input  type="submit" value="Register" />
+        </form>
+    </div>
+</body>
+</html>
+
